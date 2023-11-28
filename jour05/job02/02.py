32203fd8a2c9e2954d2a4189aa4165e5ed28bd35
haidari <130745486+zaharoudine-haidari@users.noreply.github.com>
2023-11-28 10:57:58 +0100

    4 envoi sur internet

--- jour05/job02/02.py
+++ jour05/job02/02.py
@@ -1,9 +1,6 @@
-"""
 def draw_rectangle(width,height):
     for i in range (height):
         if i == 0 or i == height - 1:
             print('-' * width)
-        else:
-            print('|' + ' '* (width - 1) + '|')
 draw_rectangle(10,3)
-"""
+
